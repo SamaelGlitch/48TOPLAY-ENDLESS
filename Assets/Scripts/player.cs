4ec4622b2cdc71bb202200b8b9f043c48f0b630a
Sansmael-Glitch <danielsamael2002@gmail.com>
2021-10-27 22:39:31 -0500

    Archivos para el proyecto

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Yincrement;
+
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+    public void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight){
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            
+        }
+    }
+}
